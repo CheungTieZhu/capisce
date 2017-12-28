96a3410e6b46da58114f22c479d8ad9932550d7b
[username] <781281662@qq.com>
2017-12-27 22:34:51 -0500

    comfirm first version is Chinese

--- capisce/Controller/UserInfo/UserOperationController.swift
+++ capisce/Controller/UserInfo/UserOperationController.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 enum userPosition: String {
-    case administator     = "Administator"
-    case manager          = "Manager"
-    case apartmentLeadr   = "Apartment Leader"
-    case teamLeader       = "Team Leader"
-    case teamMember       = "Team Member"
+    case administator     = "管理员"
+    case manager          = "经理"
+    case apartmentLeadr   = "部长"
+    case teamLeader       = "组长"
+    case teamMember       = "成员"
 }
 
 class UserOperationController: UITableViewController{
@@ -50,11 +50,11 @@ extension UserOperationController{
         getUserLevel()
         switch level {
         case 4?:
-            return 8
+            return 10
         case 3?:
-            return 7
+            return 10
         case 2?:
-            return 7
+            return 9
         case 1?:
             return 7
         case 0?:
@@ -71,16 +71,16 @@ extension UserOperationController{
             if let company = parentVC.companyDict?.company[companyIndex].company,let apartment = parentVC.companyDict?.company[companyIndex].apartment,let team = parentVC.companyDict?.company[companyIndex].team,let position = level{
                 switch indexPath.row{
                 case 0:
-                    cell.itemNameLabel.text = "company"
+                    cell.itemNameLabel.text = "公司："
                     cell.itemExpressionLabel.text = company
                 case 1:
-                    cell.itemNameLabel.text = "apartment"
+                    cell.itemNameLabel.text = "部门："
                     cell.itemExpressionLabel.text = apartment
                 case 2:
-                    cell.itemNameLabel.text = "team"
+                    cell.itemNameLabel.text = "小组："
                     cell.itemExpressionLabel.text = team
                 case 3:
-                    cell.itemNameLabel.text = "position"
+                    cell.itemNameLabel.text = "职位："
                     switch position{
                     case 4:
                         cell.itemExpressionLabel.text = userPosition.administator.rawValue
@@ -94,17 +94,23 @@ extension UserOperationController{
                         cell.itemExpressionLabel.text = userPosition.teamMember.rawValue
                     }
                 case 4:
-                    cell.itemNameLabel.text = "View Member"
-                    cell.itemExpressionLabel.text = "Member"
+                    cell.itemNameLabel.text = "查看："
+                    cell.itemExpressionLabel.text = "成员"
                 case 5:
-                    cell.itemNameLabel.text = "Manage Task"
-                    cell.itemExpressionLabel.text = "Task"
+                    cell.itemNameLabel.text = "管理："
+                    cell.itemExpressionLabel.text = "任务"
                 case 6:
-                    cell.itemNameLabel.text = "View Member"
-                    cell.itemExpressionLabel.text = "Position"
+                    cell.itemNameLabel.text = "查看："
+                    cell.itemExpressionLabel.text = "成员位置"
                 case 7:
-                    cell.itemNameLabel.text = "Manage Member"
-                    cell.itemExpressionLabel.text = "Member"
+                    cell.itemNameLabel.text = "管理："
+                    cell.itemExpressionLabel.text = "成员"
+                case 8:
+                    cell.itemNameLabel.text = "新增："
+                    cell.itemExpressionLabel.text = "小组"
+                case 9:
+                    cell.itemNameLabel.text = "新增："
+                    cell.itemExpressionLabel.text = "部门"
                 default:
                     cell.itemNameLabel.text = "company"
                     cell.itemExpressionLabel.text = company
@@ -135,6 +141,10 @@ extension UserOperationController{
             performSegue(withIdentifier: "viewLocation", sender: nil)
         case 7:
             performSegue(withIdentifier: "ManageMember", sender: nil)
+        case 8:
+            performSegue(withIdentifier: "addTeam", sender: nil)
+        case 7:
+            performSegue(withIdentifier: "addDepartment", sender: nil)
         default:
             print("WTF")
         }
